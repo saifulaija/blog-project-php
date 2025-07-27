cea96ff9143ea3cc55bd4318ba5009b8a5405f5c
saifulaija <saifulaija2023@gmail.com>
2025-07-27 21:59:14 +0600

    dashboard template implement done

---
+++ admin/template.php
@@ -0,0 +1,19 @@
+<?php include_once('includes/head.php') ?>
+
+<body class="sb-nav-fixed">
+    <?php include_once('includes/topnav.php') ?>
+    <div id="layoutSidenav">
+        <?php include_once('includes/sidenav.php') ?>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid">
+
+                </div>
+            </main>
+            <?php include_once('includes/footer.php')?>
+        </div>
+    </div>
+    <?php include_once('includes/script.php') ?>
+</body>
+
+</html>
